23df318342be7c99875dd5ba7973bd530a791d7e
Muhamad Syalum <msyalum@gmail.com>
2024-09-27 15:24:50 +0700

    Initial commit

---
+++ src/Components/Header/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+const Header = () => {
+    return (
+        <>
+            <div className='headerWrapper'>
+                <div className='top-strip bg-blue'>
+                    <div className='container'>
+                        <h1 className='mb-0 mt-0 text-center'>Lembaga Sertifikasi Profesi</h1>
+                        <h1 className='text-center'>Administrasi Perkantoran Indonesia</h1>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Header;
